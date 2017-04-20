065b6a832f8692da3ca96310942b292b10041838
splicer818 <jkingsley2000us@yahoo.com>
2017-04-19 22:04:15 -0700

    Version 1 Adds a scheduled tasks. Meant for remoting

---
+++ add-task.ps1
@@ -0,0 +1,38 @@
+﻿function Add-Task{
+  param
+  (
+  
+     [Parameter(Mandatory=$True)]
+     [string]$program,
+     [string]$taskName,
+     [string]$argument,
+     [string]$userName,
+     [string]$password,
+     [Parameter(Mandatory=$False)]
+     [string]$workingDirectory
+  
+  )
+  
+     process {
+            
+          if ($workingDirectory) { 
+          
+               $action = New-ScheduledTaskAction -Execute $program -Argument $argument -WorkingDirectory $workingDirectory
+          }
+            
+          else { 
+          
+               $action = New-ScheduledTaskAction -Execute $program -Argument $argument 
+               
+          }
+
+          $settings = New-ScheduledTaskSettingsSet
+     
+          $inputObject = New-ScheduledTask -Action $action -Settings $settings 
+            
+          Register-ScheduledTask -TaskName $taskName -InputObject $inputObject -User $user -Password $password
+  
+     }
+
+}
+
